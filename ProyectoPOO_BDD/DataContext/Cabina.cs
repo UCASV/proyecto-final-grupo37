f7637c36f3461c673d249b081767a7f727dc012e
DiegoJacob0 <00043719@uca.edu.sv>
2021-06-29 17:07:38 -0600

    Part2, CreateUser

---
+++ ProyectoPOO_BDD/DataContext/Cabina.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProyectoPOO_BDD.DataContext
+{
+    public partial class Cabina
+    {
+        public Cabina()
+        {
+            CabinaxEmpleados = new HashSet<CabinaxEmpleado>();
+        }
+
+        public int IdCabina { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+
+        public virtual ICollection<CabinaxEmpleado> CabinaxEmpleados { get; set; }
+    }
+}
